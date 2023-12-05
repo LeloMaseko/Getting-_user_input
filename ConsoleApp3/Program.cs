188a748abdeecc21aff3c416dc6e6462736b74c1
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 14:14:09 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi" + name);
+            Console.WriteLine("how old are you? ");
+            
+            String age = Console.ReadLine();
+            Console.WriteLine("you are " + age);
+            Console.ReadLine();
+        }
+    }
+}
+      
